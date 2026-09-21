a26d4ab696f63afe80d600424e9b9ade6e228ffb
Marcos Willian <marcoswillianl1987@gmail.com>
2026-09-21 16:43:34 -0400

    classes a mais e sintaxe
    Acrescentei mais duas classes e corrigi a sintaxe.

--- main.py
+++ main.py
@@ -22,13 +22,32 @@ class Aluno(Pessoa):
     pass
 
 class Professor(Pessoa):
-  def __init__(self, nome, email, data_nascimento, endereco, cpf, telefone, contrato, modulos, ativo=True):
+  def __init__(self, nome, email, data_nascimento, endereco, cpf, telefone, contrato, modulos = None, ativo=True):
     super().__init__(nome, email, data_nascimento, endereco, cpf, telefone, ativo)
     self.contrato = contrato
-    self.modulos = []
+    self.modulos = modulos if modulos is not None else []
 
     def registrar_aula(self):
       pass
 
     def adicionar_modulo(self):
       pass
+
+class Modulo:
+  def __init__(self, nome, duracao, cursos=None, professores=None):
+    self.nome = nome
+    self.duracao = duracao
+    self.cursos = cursos if cursos is not None else []
+    self.professores = professores if professores is not None else []
+
+  def registrar_notas(self):
+    pass
+
+  def registrar_faltas(self):
+    pass
+
+class Curso:
+  def __init__(self, nome, duracao, modulos=None):
+    self.nome = nome
+    self.duracao = duracao
+    self.modulos = modulos if modulos is not None else []
