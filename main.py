2c8bff43cb6906588ff23c39f0c3758f979e389c
Marcos Willian <marcoswillianl1987@gmail.com>
2026-09-21 16:23:39 -0400

    classes
    Algumas classes provisórias

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class Pessoa:
-  def __init__(self, nome, email, data_nascimento, endereco, cpf, telefone, ativo):
+  def __init__(self, nome, email, data_nascimento, endereco, cpf, telefone, ativo=True):
     self.nome = nome
     self.email = email
     self.data_nascimento = data_nascimento
@@ -8,8 +8,27 @@ class Pessoa:
     self.telefone = telefone
     self.ativo = ativo
 
-  def cadastrar_pessoa():
+  def cadastrar_pessoa(self):
     pass
 
 class Aluno(Pessoa):
-  pass
+  def __init__(self, nome, email, data_nascimento, endereco, cpf, telefone, matricula, curso, turma, ativo=True,):
+    super().__init__(nome, email, data_nascimento, endereco, cpf, telefone, ativo)
+    self.matricula = matricula
+    self.curso = curso
+    self.turma = turma
+
+  def fazer_matricula(curso):
+    pass
+
+class Professor(Pessoa):
+  def __init__(self, nome, email, data_nascimento, endereco, cpf, telefone, contrato, modulos, ativo=True):
+    super().__init__(nome, email, data_nascimento, endereco, cpf, telefone, ativo)
+    self.contrato = contrato
+    self.modulos = []
+
+    def registrar_aula(self):
+      pass
+
+    def adicionar_modulo(self):
+      pass
