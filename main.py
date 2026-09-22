4812e742583b20f1a3f80150b474799c01cdb07a
Marcos Willian <marcoswillianl1987@gmail.com>
2026-09-22 16:43:45 -0400

    mudanças
    Algumas mudanças e uma classe nova

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ class Pessoa:
   def cadastrar_pessoa(self):
     pass
 
+  def visualizar_perfil(self):
+    pass
+
+  def configurar_perfil(self):
+    pass
+
+  def remover_perfil(self):
+    pass
+
+
 class Aluno(Pessoa):
   def __init__(self, nome, email, data_nascimento, endereco, cpf, telefone, matricula, curso, turma, ativo=True,):
     super().__init__(nome, email, data_nascimento, endereco, cpf, telefone, ativo)
@@ -18,7 +28,7 @@ class Aluno(Pessoa):
     self.curso = curso
     self.turma = turma
 
-  def fazer_matricula(curso):
+  def fazer_matricula(self, curso):
     pass
 
 class Professor(Pessoa):
@@ -27,10 +37,10 @@ class Professor(Pessoa):
     self.contrato = contrato
     self.modulos = modulos if modulos is not None else []
 
-    def registrar_aula(self):
+  def registrar_aula(self):
       pass
 
-    def adicionar_modulo(self):
+  def adicionar_modulo(self):
       pass
 
 class Modulo:
@@ -51,3 +61,31 @@ class Curso:
     self.nome = nome
     self.duracao = duracao
     self.modulos = modulos if modulos is not None else []
+
+  def 
+
+class Sistema: 
+  def __init__(self, alunos=None, professores=None, turmas=None, cursos=None, modulos=None):
+    self.alunos = alunos if alunos is not None else []
+    self.professores = professores if professores is not None else []
+    self.turmas = turmas if turmas is not None else []
+    self.cursos = cursos if cursos is not None else []
+    self.modulos = modulos if modulos is not None else []
+
+  def exibir_menu(self):
+    pass
+
+  def cadastrar_aluno(self):
+    pass 
+
+  def cadastrar_professor(self):
+    pass
+
+  def cadastrar_turmas(self):
+    pass
+
+  def cadastrar_cursos(self):
+    pass 
+
+  def cadastrar_modulos(self):
+    pass
